9168689e4bbcb03f3e907a7fe178b21a94e71b6c
ZipNet <ZipNet@ZipNet-PC>
2014-07-22 16:34:21 -0300

    Classes bean

--- sistema-audizio/src/sistema/audizio/bean/Pessoa.java
+++ sistema-audizio/src/sistema/audizio/bean/Pessoa.java
@@ -11,6 +11,61 @@ package sistema.audizio.bean;
  * @author ZipNet
  */
 public class Pessoa {
-   
-    
+	
+        String nome; 
+        String fone;
+        String endereco;
+        String bairro;
+        String cidade;
+        String banco;
+        int num;
+        
+        
+    public String getFone() {
+        return fone;
+    }
+
+    public void setFone(String fone) {
+        this.fone = fone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getBanco() {
+        return banco;
+    }
+
+    public void setBanco(String banco) {
+        this.banco = banco;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
 }
---
+++ sistema-audizio/src/sistema/audizio/bean/Veiculo.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sistema.audizio.bean;
+
+/**
+ *
+ * @author ZipNet
+ */
+public class Veiculo {
+    
+    String ano;
+    String modelo;
+    int chassi;
+    String placa;
+    String marca;
+    
+    public String getAno() {
+        return ano;
+    }
+
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getChassi() {
+        return chassi;
+    }
+
+    public void setChassi(int chassi) {
+        this.chassi = chassi;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+   
+}
